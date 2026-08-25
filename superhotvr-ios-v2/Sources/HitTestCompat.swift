42ac401f4fe0a28ca09a3f8b95abc91c85f32e6d
pedrosenhas746-ship-it <pedrosenhas746@gmail.com>
2026-08-25 17:54:05 -0300

    Fix SceneKit hit-test option keys

---
+++ superhotvr-ios-v2/Sources/HitTestCompat.swift
@@ -0,0 +1,6 @@
+import SceneKit
+
+extension String {
+    static var searchMode: String { SCNHitTestOption.searchMode.rawValue }
+    static var backFaceCulling: String { SCNHitTestOption.backFaceCulling.rawValue }
+}
