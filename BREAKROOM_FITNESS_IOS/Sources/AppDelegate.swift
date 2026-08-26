8030a50f69937467d14d1f2c5bb706607f6ff2e7
pedrosenhas746-ship-it <pedrosenhas746@gmail.com>
2026-08-26 19:45:17 -0300

    Launch BREAKROOM Fitness V2 by default

--- BREAKROOM_FITNESS_IOS/Sources/AppDelegate.swift
+++ BREAKROOM_FITNESS_IOS/Sources/AppDelegate.swift
@@ -7,7 +7,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         let w = UIWindow(frame: UIScreen.main.bounds)
-        w.rootViewController = FitnessGameViewController()
+        w.rootViewController = FitnessGameV2ViewController()
         w.makeKeyAndVisible()
         window = w
         return true
